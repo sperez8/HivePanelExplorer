fdd72e74c7e5093bf364b53c56747ce4a2a422e3
sperez8 <karatezeus21@gmail.com>
2014-06-05 15:36:30 -0700

    reorganized files.

--- scripts/main.py
+++ scripts/main.py
@@ -6,15 +6,19 @@ by sperez
 Contains HivePlot module used to produce the javascript files
 needed to make a hive plot in D3 using Mike Bolstock's D3 hive module
 '''
-
+#library imports
 import os
 import sys
 import csv
 import numpy as np
+
+#module imports
+from ..data import html_items # not working :(
 
 class HivePlotter():
     '''creates files necessary to make a hive plot in D# given 
         a network with nodes and links stored in a csv file'''
     
     def __init__(self):
+        print htmlContainer
         return None
