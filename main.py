061ca9de5a05e83efc6fac6f0aaf4d4ef6d39b91
sperez8 <karatezeus21@gmail.com>
2014-06-05 09:12:54 -0700

    create hiveplotter class

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+created  03/06/2014
+
+by sperez
+
+Contains HivePlot module used to produce the javascript files
+needed to make a hive plot in D3 using Mike Bolstock's D3 hive module
+'''
+
+import os,sys
+import csv
+import numpy as np
+
+class HivePlotter():
+    '''creates files necessary to make a hive plot in D# given 
+        a network with nodes and links stored in a csv file'''
+    
+    def __init__(self):
+        return None
