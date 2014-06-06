50f39100f5cfd73b350364d2e6b2d3f93e6bcddd
sperez8 <karatezeus21@gmail.com>
2014-06-06 12:03:02 -0700

    added example for edges csv file. Hiveplotter now gets edges from file, converts types, and avoids "lists of one lists" for node/edge properties

--- scripts/main.py
+++ scripts/main.py
@@ -18,7 +18,7 @@ class HivePlotter():
     '''creates files necessary to make a hive plot in D# given 
         a network with nodes and links stored in a csv file'''
     
-    def __init__(self, debug = False):
+    def __init__(self, debug = True):
         self.debug = debug
         return None
     
@@ -31,6 +31,8 @@ class HivePlotter():
         nodeProperties = []
         for column in data[:,2:].T:
             nodeProperties.append(list(column))
+        if len(nodeProperties) == 1: 
+            nodeProperties=nodeProperties[0] #avoid having a list of one list when there is only 1 property 
         #transform it into the right data type
         self.nodes = self.convert_type(nodes)
         self.nodeNames = self.convert_type(nodeNames)
@@ -41,6 +43,31 @@ class HivePlotter():
             print self.nodeNames
             print self.nodeProperties
         return None
+
+    def get_edges(self,inputFile, delimiter = ','):
+        '''gets edges and their properties from csv file'''
+        data = np.genfromtxt(inputFile, delimiter=delimiter, skiprows = 1, dtype='str')
+        #get all the edge data
+        sources = data[:,0]        
+        targets = data[:,1]
+        edgeProperties = []
+        for column in data[:,2:].T:
+            edgeProperties.append(list(column))
+        
+        if len(edgeProperties) == 1: 
+            edgeProperties=edgeProperties[0] #avoid having a list of one list when there is only 1 property 
+            
+        #transform it into the right data type
+        self.sources = self.convert_type(sources)
+        self.targets = self.convert_type(targets)
+        self.edgeProperties = [self.convert_type(p) for p in edgeProperties]
+        
+        if self.debug:
+            print self.sources
+            print self.targets
+            print self.edgeProperties
+        return None
+
 
     @staticmethod
     def convert_type(data):
@@ -60,3 +87,4 @@ class HivePlotter():
 # I test stuff here
 hive = HivePlotter()
 hive.get_nodes('tests/test_nodes_friends.csv')
+hive.get_edges('tests/test_edges_friends.csv')
