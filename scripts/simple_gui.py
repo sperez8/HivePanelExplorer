bba8ce35b0f7ef263fe98ab91ab4b8f6b67caacb
sperez8 <karatezeus21@gmail.com>
2014-06-20 10:14:22 -0700

    added options menu for debug. storing options in new file

--- scripts/simple_gui.py
+++ scripts/simple_gui.py
@@ -10,6 +10,8 @@ import sys
 import os
 from Tkinter import *
 from main import *
+from gui_options import *
+
 _cur_dir = os.path.dirname(os.path.realpath(__file__))
 _root_dir = os.path.dirname(_cur_dir)
 sys.path.insert(0, _root_dir)
@@ -26,16 +28,24 @@ root.geometry("600x400")
 def callback():
     nodefile = nodes.get()
     edgefile = edges.get()
+    debug = debug.get()
     hive = make_hive(nodefile, edgefile, debug)
     make_html(title, hive)
     
-def makeentry(parent, caption, width=None, **options):
+def make_entry(parent, caption, width=None, **options):
     Label(parent, text=caption).pack(side=LEFT)
     entry = Entry(parent, **options)
     if width:
         entry.config(width=width)
     entry.pack()
     return entry
+
+def make_options(parent, options):
+    var = StringVar(parent)
+    var.set(options[0]) #default value
+    w = apply(OptionMenu, (parent, var) + tuple(options))
+    w.pack()
+    
 
 #add a label
 app = Frame(root)
@@ -49,14 +59,14 @@ b.pack()
 b.configure(text = "Submit", width=20, command=callback)
 
 #get node and edge file
-nodes = makeentry(app, "Nodes:", 60)
-edges = makeentry(app, "Edges:", 60)
-
-debug = makeentry(app, "verbatim:", 10)
+nodes = make_entry(app, "Nodes:", 60)
+edges = make_entry(app, "Edges:", 60)
 
 nodes.get()
 edges.get()
 
+#make different option menus
+debug  = make_options(app, debugOptions)
 
 #run gui
 root.mainloop()
