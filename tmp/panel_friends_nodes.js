ae003811a95d31435c411454249c7dca1e027dfe
sperez8 <karatezeus21@gmail.com>
2014-10-10 14:09:19 -0700

    adding current prototype for hive panel in /tmp folder

---
+++ tmp/panel_friends_nodes.js
@@ -0,0 +1,13 @@
+var nodes = [
+  {degree: 2, pos: 0.45, name: 'Alice', Gender: 2, Height: 1},
+  {degree: 1, pos: 0.25, name: 'Bob', Gender: 1, Height: 0.2},
+  {degree: 1, pos: 1.0, name: 'Cam', Gender: 1, Height: 0.9},
+  {degree: 0, pos: 0.0, name: 'Daniella', Gender: 2, Height: 0.8},
+  {degree: 0, pos: 0.0, name: 'Eric', Gender: 1, Height: 1},
+  {degree: 2, pos: 0.6, name: 'Fatima', Gender: 2, Height: 0.9},
+  {degree: 1, pos: 0.25, name: 'George', Gender: 0, Height: 0.9},
+  {degree: 0, pos: 0.0, name: 'Harry', Gender: 0, Height: 0.7},
+  {degree: 1, pos: 0.75, name: 'Isabelle', Gender: 0, Height: 0.3},
+  {degree: 1, pos: 0.5, name: 'Jack', Gender: 1, Height: 0.5},
+  {degree: 0, pos: 0.0, name: 'Kelly', Gender: 2, Height: 0.1},
+];
