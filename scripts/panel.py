42f5da42f32abb91d6a098a00ab9ee52875575b5
sperez8 <karatezeus21@gmail.com>
2014-10-14 11:46:25 -0700

    panel class makes hive plots for each rule pair

--- scripts/panel.py
+++ scripts/panel.py
@@ -11,7 +11,11 @@ import sys
 import os
 import numpy as np
 
-from hive import Hive
+from hive import *
+
+_cur_dir = os.path.dirname(os.path.realpath(__file__))
+_root_dir = os.path.dirname(_cur_dir)
+sys.path.insert(0, _root_dir)
 
 SIZE = (2,2) #assignment by position rules
 ASSIGNMENT_RULES = ('degree', 'centrality')
@@ -24,6 +28,7 @@ class Panel():
                  size = SIZE,
                  debug = True,
                  numAxes = 3,
+                 doubleAxes = False,
                  assignmentRules = ASSIGNMENT_RULES,
                  positionRules = POSITION_RULES,
                  edgePalette = EDGE_PALETTE,
@@ -35,6 +40,7 @@ class Panel():
         self.debug = debug
         self.size = size
         self.numAxes = numAxes
+        self.doubleAxes = False
         self.assignmentRules = assignmentRules
         self.positionRules = positionRules
         self.edgePalette = edgePalette
@@ -46,21 +52,53 @@ class Panel():
                 
         return None
     
-    def make_panel(self):
+    def make_panel(self, nodefile, edgefile):
         '''makes the hive plots and returns the needed assignment values'''
         if not self.size_rules_agree():
-            print "the size and rules specified don't agree"
+            print "The number of rules and the specified size of the panel don't agree."
             sys.exit()
+        rulePairs = self.cross_rules(self.assignmentRules, self.positionRules)
+        Hives = {}
+        for a,p in rulePairs:
+            print a,p
+            hive = Hive(debug = self.debug,
+                    numAxes = self.numAxes,
+                    doubleAxes = self.doubleAxes, 
+                    axisAssignRule = a, 
+                    axisPositRule = p,
+                    edgePalette = self.edgePalette, 
+                    edgeStyleRule = self.edgeStyleRule,
+                    nodeColor = self.nodeColor
+                    )
+            
+            hive.make_hive(nodefile, edgefile) 
+            Hives[(a,p)] = hive
+            hive = None
+        
+        return Hives   
         
     def size_rules_agree(self):
-        if size[0] != len(assignmentRules):
+        if self.size[0] != len(self.assignmentRules):
             return False
-        if size[1] != len(positionRules):
+        if self.size[1] != len(self.positionRules):
             return False
         else:
             return True
         
+    @staticmethod
+    def cross_rules(asgRules, posRules):
+        rules = []
+        for a in asgRules:
+            for p in posRules:
+                rules.append((a,p))
+        return rules
 
+if __name__ == "__main__":
+    '''testing purposes'''
+    P = Panel()
+    NODES = os.path.join(_root_dir, 'tests', 'test_nodes_friends.txt')
+    EDGES = os.path.join(_root_dir, 'tests', 'test_edges_friends.txt')
+    P.make_panel(NODES, EDGES)
 
 
 
