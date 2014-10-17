aa6e8a0d16a2bff8481e7593d7172d41a401beae
sperez8 <karatezeus21@gmail.com>
2014-10-17 11:36:56 -0700

    created a new layout for the panel. Soon it will be incorporated and generated by write_html.py

---
+++ tmp/friends_panel_test_nodes.js
@@ -0,0 +1,13 @@
+var nodes = [
+    {name: 'Alice', Gender_axis: 2, clustering_pos: 0.45, degree_axis: 2, betweeness_pos: 1.0, Height_pos: 1.0, Gender: 'girl', Height: '1'},
+    {name: 'Bob', Gender_axis: 1, clustering_pos: 0.25, degree_axis: 1, betweeness_pos: 0.854, Height_pos: 0.2, Gender: 'boy', Height: '0.2'},
+    {name: 'Cam', Gender_axis: 1, clustering_pos: 1.0, degree_axis: 1, betweeness_pos: 0.042, Height_pos: 0.9, Gender: 'boy', Height: '0.9'},
+    {name: 'Daniella', Gender_axis: 2, clustering_pos: 0.0, degree_axis: 0, betweeness_pos: 0.167, Height_pos: 0.8, Gender: 'girl', Height: '0.8'},
+    {name: 'Eric', Gender_axis: 1, clustering_pos: 0.0, degree_axis: 0, betweeness_pos: 0.125, Height_pos: 1.0, Gender: 'boy', Height: '1'},
+    {name: 'Fatima', Gender_axis: 2, clustering_pos: 0.6, degree_axis: 2, betweeness_pos: 0.75, Height_pos: 0.9, Gender: 'girl', Height: '0.9'},
+    {name: 'George', Gender_axis: 0, clustering_pos: 0.25, degree_axis: 1, betweeness_pos: 0.792, Height_pos: 0.9, Gender: 'alien', Height: '0.9'},
+    {name: 'Harry', Gender_axis: 0, clustering_pos: 0.0, degree_axis: 0, betweeness_pos: 0.063, Height_pos: 0.7, Gender: 'alien', Height: '0.7'},
+    {name: 'Isabelle', Gender_axis: 0, clustering_pos: 0.75, degree_axis: 1, betweeness_pos: 0.229, Height_pos: 0.3, Gender: 'alien', Height: '0.3'},
+    {name: 'Jack', Gender_axis: 1, clustering_pos: 0.5, degree_axis: 1, betweeness_pos: 0.229, Height_pos: 0.5, Gender: 'boy', Height: '0.5'},
+    {name: 'Kelly', Gender_axis: 2, clustering_pos: 0.0, degree_axis: 0, betweeness_pos: 0.0, Height_pos: 0.1, Gender: 'girl', Height: '0.1'},
+];
