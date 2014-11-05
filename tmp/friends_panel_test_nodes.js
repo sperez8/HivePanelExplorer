67022d71959b2c1e1ea73d585a49df14f060b482
sperez8 <karatezeus21@gmail.com>
2014-11-04 16:45:24 -0800

    undoing and redoing multiple color rules works!!!!

--- tmp/friends_panel_test_nodes.js
+++ tmp/friends_panel_test_nodes.js
@@ -1,9 +1,9 @@
 var nodes = [
-    {name: 'Alice', degree_axis: 2, clustering_pos: 0.45, Height_pos: 1.0, betweeness_pos: 1.0, Gender_axis: 2, Height_axis: 2, Gender: 'girl', Height: '1'},
+    {name: 'Alice', degree_axis: 2, clustering_pos: 0.45, Height_pos: 3.0, betweeness_pos: 1.0, Gender_axis: 2, Height_axis: 2, Gender: 'girl', Height: '1'},
     {name: 'Bob', degree_axis: 1, clustering_pos: 0.25, Height_pos: 0.2, betweeness_pos: 0.854, Gender_axis: 1, Height_axis: 0, Gender: 'boy', Height: '0.2'},
     {name: 'Cam', degree_axis: 1, clustering_pos: 1.0, Height_pos: 0.9, betweeness_pos: 0.042, Gender_axis: 1, Height_axis: 1, Gender: 'boy', Height: '0.9'},
     {name: 'Daniella', degree_axis: 0, clustering_pos: 0.0, Height_pos: 0.8, betweeness_pos: 0.167, Gender_axis: 2, Height_axis: 1, Gender: 'girl', Height: '0.8'},
-    {name: 'Eric', degree_axis: 0, clustering_pos: 0.0, Height_pos: 1.0, betweeness_pos: 0.125, Gender_axis: 1, Height_axis: 2, Gender: 'boy', Height: '1'},
+    {name: 'Eric', degree_axis: 0, clustering_pos: 0.0, Height_pos: 2.0, betweeness_pos: 0.125, Gender_axis: 1, Height_axis: 2, Gender: 'boy', Height: '1'},
     {name: 'Fatima', degree_axis: 2, clustering_pos: 0.6, Height_pos: 0.9, betweeness_pos: 0.75, Gender_axis: 2, Height_axis: 1, Gender: 'girl', Height: '0.9'},
     {name: 'George', degree_axis: 1, clustering_pos: 0.25, Height_pos: 0.9, betweeness_pos: 0.792, Gender_axis: 0, Height_axis: 1, Gender: 'alien', Height: '0.9'},
     {name: 'Harry', degree_axis: 0, clustering_pos: 0.0, Height_pos: 0.7, betweeness_pos: 0.063, Gender_axis: 0, Height_axis: 1, Gender: 'alien', Height: '0.7'},
