d9477746b607481197895e8b861ad239ae3eb203
sperez8 <karatezeus21@gmail.com>
2014-11-10 14:48:16 -0800

    mend
    fixed filter. works lit it is supposed now. Reorganized webfiles into html layout, css, js methods and js parameter files

---
+++ tmp/panel_parameters.js
@@ -0,0 +1,34 @@
+
+  // ****************************************** //
+
+
+ //         USER DEFINED PARAMETERS            //
+
+
+// ****************************************** //
+
+var SVGTitle = 'Friends Forever' + ' Hive Panel'
+
+var num_axis = 3
+    angle = [0.0001, 2.09, 4.19]
+
+var nodeColor = 'darkgrey'
+    edgeColor = ['darkgrey']
+    linkwidth = 1.3
+    oplink = 0.8
+    opnode = 0.8
+
+var nodesize = 5
+    nodestroke = 0.4
+    nodestrokecolor = "grey"
+
+var linkfill = "none"
+    bkgcolor = "white"
+
+var hoverOverTime = 900
+
+
+var columntraits = ["Gender", "degree"];
+    rowtraits = ["Gender", "Height"];
+    asgScales = {};
+    posScales = {};
