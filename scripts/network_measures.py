26a8dc6a1f5ffa14e38da4ba76d2fd31922cd7cf
Sarah Perez <sperez@shuttle.mi.microbiology.ubc.ca>
2014-11-25 12:40:30 -0800

    measures per component and per node can be calculated.

--- scripts/network_measures.py
+++ scripts/network_measures.py
@@ -25,6 +25,7 @@ from make_network import *
 NODES = os.path.join(_root_dir, 'tests', 'test_nodes_friends.txt')
 EDGES = os.path.join(_root_dir, 'tests', 'test_edges_friends.txt')
 
+
 def main(*argv):
     '''handles user input and runs plsa'''
     parser = argparse.ArgumentParser(description='This scripts produces an interactive hive panel.')
@@ -42,21 +43,49 @@ def main(*argv):
 
     G = import_graph(nodeFile,edgeFile)
 
-    print "\n\n\nMade the networkx graph:"
-    print G.nodes(data=True)
-    print G.edges(data=True)
+    print "\nMade the networkx graph."
+
     return G
 
-def measure_all(G):
+def measure_whole(G):
     '''measure all interesting global network measures'''
-    measure = {}
+    measures = {}
+
+    measures['components'] = [len(x) for x in nx.connected_components(G)]
+    measures['d'] =  list(G.degree().values())
+    measures['bc'] =  list(nx.betweenness_centrality(G).values())
+    measures['cc'] =  list(nx.closeness_centrality(G).values())
+    measures['clustering'] =  list(nx.clustering(G).values())
     
+
     return measures
 
+def measure_component(G):
+    '''measure all interesting global network measures'''
+    measuresC = {}
 
+    components = nx.connected_component_subgraphs(G)
+    for i,c in enumerate(components):
+        measuresC[i] = {}
+        measuresC[i]['d'] = list(c.degree().values())
+        measuresC[i]['bc'] =  list(nx.betweenness_centrality(c).values())
+        measuresC[i]['cc'] =  list(nx.closeness_centrality(c).values())
+        measuresC[i]['clustering'] =  list(nx.clustering(c).values())
     
+
+    return measuresC
+
+
 if __name__ == "__main__":
     '''testing purposes'''
-    main(*sys.argv[1:])
+    G = main(*sys.argv[1:])
+    measures = measure_whole(G)
+    measures.update(measure_component(G))
+
+    '''Use  commands like:
+    nx.attribute_mixing_matrix(G, 'Gender')
+    and
+    nx.attribute_assortativity_coefficient(G, 'Gender')
+    to find the assortativity between node attributes'''
 
 
