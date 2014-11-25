d15b2273279d53fb645564110a480b3f9e8ce1e7
Sarah Perez <sperez@shuttle.mi.microbiology.ubc.ca>
2014-11-25 11:43:14 -0800

    import csv graph adds attributes to nodes and edges

--- scripts/network_measures.py
+++ scripts/network_measures.py
@@ -9,31 +9,42 @@ Contains functions used by hive class to measure things like network properties
 #library imports
 import sys
 import os
+import argparse
 import numpy as np
 from math import pi
 import hive as hive
+
 
 _cur_dir = os.path.dirname(os.path.realpath(__file__))
 _root_dir = os.path.dirname(_cur_dir)
 sys.path.insert(0, _root_dir)
 
 import networkx as nx
+from make_network import *
 
+NODES = os.path.join(_root_dir, 'tests', 'test_nodes_friends.txt')
+EDGES = os.path.join(_root_dir, 'tests', 'test_edges_friends.txt')
 
-def make_graph_from_graphml(graphmlFile):
-    #parse graphml file
-    G = nx.read_graphml(graphmlFile)
-    return G
-
-def make_graph_from_2_files(nodeFile, edgeFile):
-    '''make a networkx graph from a csv or tsv using methods from the hive class'''
-    hive = hive.Hive()
-    hive.get_nodes(nodeFile)
-    hive.get_edges(edgeFile)
+def main(*argv):
+    '''handles user input and runs plsa'''
+    parser = argparse.ArgumentParser(description='This scripts produces an interactive hive panel.')
+    parser.add_argument('-n', help='The node file', default = NODES)
+    parser.add_argument('-e', help='The edge file', default = EDGES)
+    args = parser.parse_args()
     
-    sources, targets, nodes = hive.sources, hive.targets, hive.nodes
-    G = make_graph(sources, targets, nodes)
-    
+    if (args.n == '' and args.e != '') or (args.n != '' and args.e == ''):
+        print "\n***You must specify both a node and an edge file if specifying either.***\n"
+        parser.print_help()
+        sys.exit()
+        
+    nodeFile = args.n
+    edgeFile = args.e
+
+    G = import_graph(nodeFile,edgeFile)
+
+    print "\n\n\nMade the networkx graph:"
+    print G.nodes(data=True)
+    print G.edges(data=True)
     return G
 
 def measure_all(G):
@@ -42,72 +53,10 @@ def measure_all(G):
     
     return measures
 
-def convert_graphml(graphmlFile):
-    G = make_graph_from_graphml(graphmlFile)
-    sources, targets, edgeProperties = zip(*G.edges(data=True))
-    fileName = graphmlFile.split('.graphml')[0]
-    nodeFile = fileName+'_nodes.csv'
-    edgeFile = fileName+'_edges.csv'
-    nf = open(nodeFile,'w')
-    keys = []
-    for node, nodeProperties in G.nodes(data=True):
-        print nodeProperties.keys()
-        new_keys = nodeProperties.keys()
-        if new_keys != keys:
-            keys.extend(new_keys)
-    
-    keys = set(keys)
-    print keys
-    
-    #write header
-    nf.write('Node'+','+','.join(keys))
-    
-    for node, nodeProperties in G.nodes(data=True):
-        row = []
-        row.append(node)
-        for k in keys:
-            if k in nodeProperties.keys():
-                row.append(str(nodeProperties[k]).replace(',', ';'))
-            else:
-                row.append('None')
-        print row
-        nf.write('\n' + ','.join(row))
-    
-    nf.close()
-    
-    ef = open(edgeFile,'w')
-    keys = []
-    for source,target, edgeProperties in G.edges(data=True):
-        print edgeProperties.keys()
-        new_keys = edgeProperties.keys()
-        if new_keys != keys:
-            keys.extend(new_keys)
-    
-    keys = set(keys)
-    print keys
-    
-    ef.write('source' + ',' + 'target' + ',' +','.join(keys))
-    for source, target, edgeProperties in G.edges(data=True):
-        row = []
-        row.append(source)
-        row.append(target)
-        for k in keys:
-            if k in edgeProperties.keys():
-                row.append(str(edgeProperties[k]))
-            else:
-                row.append('None')
-        print row
-        ef.write('\n' + ','.join(row))
-    
-    print "writing nodefile", nodeFile
-    print "writing edgefile", edgeFile
-    return None    
 
-file = "C:\Users\Sarah\Dropbox\\0-HalLab\Hive panel examples\C.elegans\c.elegans.herm_pharynx_1.graphml"
-print file
-convert_graphml(file)
-
-
-
+    
+if __name__ == "__main__":
+    '''testing purposes'''
+    main(*sys.argv[1:])
 
 
