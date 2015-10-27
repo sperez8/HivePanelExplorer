c8e422df252843d0a83c4362a812069244b6d6cc
Sarah Perez <karatezeus21@gmail.com>
2015-10-27 15:13:43 -0700

    reorganized files and using local copy of networkx package

---
+++ test/friends/friends_nodes_nodes.js
@@ -0,0 +1,15 @@
+var SVGTitle = "friends nodes Hive Panel"
+var nodes = [
+    {name: "Matt", gender: "boy", height: "63", degree: "5", clustering: "0.6", betweenness_centrality: "0.0587878787879", closeness_centrality: "0.578947368421", component_membership: "0"},
+    {name: "Zill", gender: "alien", height: "52", degree: "5", clustering: "0.3", betweenness_centrality: "0.274848484848", closeness_centrality: "0.647058823529", component_membership: "0"},
+    {name: "Emma", gender: "girl", height: "77", degree: "1", clustering: "0.0", betweenness_centrality: "0.0", closeness_centrality: "0.407407407407", component_membership: "0"},
+    {name: "Zed", gender: "alien", height: "60", degree: "3", clustering: "0.333333333333", betweenness_centrality: "0.0666666666667", closeness_centrality: "0.52380952381", component_membership: "0"},
+    {name: "Vivian", gender: "girl", height: "73", degree: "3", clustering: "0.333333333333", betweenness_centrality: "0.0363636363636", closeness_centrality: "0.458333333333", component_membership: "0"},
+    {name: "Alice", gender: "girl", height: "70", degree: "4", clustering: "0.333333333333", betweenness_centrality: "0.224242424242", closeness_centrality: "0.611111111111", component_membership: "0"},
+    {name: "David", gender: "boy", height: "72", degree: "3", clustering: "0.666666666667", betweenness_centrality: "0.0121212121212", closeness_centrality: "0.478260869565", component_membership: "0"},
+    {name: "Zorb", gender: "alien", height: "61", degree: "4", clustering: "0.666666666667", betweenness_centrality: "0.0360606060606", closeness_centrality: "0.55", component_membership: "0"},
+    {name: "Isaac", gender: "boy", height: "65", degree: "1", clustering: "0.0", betweenness_centrality: "0.0", closeness_centrality: "0.44", component_membership: "0"},
+    {name: "Alex", gender: "girl", height: "75", degree: "2", clustering: "1.0", betweenness_centrality: "0.0", closeness_centrality: "0.423076923077", component_membership: "0"},
+    {name: "Zans", gender: "alien", height: "57", degree: "7", clustering: "0.285714285714", betweenness_centrality: "0.389696969697", closeness_centrality: "0.733333333333", component_membership: "0"},
+    {name: "Joe", gender: "boy", height: "66", degree: "4", clustering: "0.666666666667", betweenness_centrality: "0.0284848484848", closeness_centrality: "0.52380952381", component_membership: "0"},
+];
---
+++ test/friends/friends_nodes_parameters.js
@@ -0,0 +1,26 @@
+  // ****************************************** //
+
+
+ //         USER DEFINED PARAMETERS            //
+
+
+// ****************************************** //
+
+var numAxes = 3
+    doubleAxes = false
+
+var nodeColor = 'darkgrey'
+    edgeColor = ['darkgrey']
+    linkwidth = 1.2
+    oplink = 0.3
+    opnode = 0.2
+    opnode_more = 0.6
+    nodesize = 5
+    nodestroke = 0.2
+    nodestrokecolor = "grey"
+
+var columntraits = ["gender","degree"];
+    rowtraits = ["height","clustering"];
+
+var columnTraitScales = {"gender":"linear","degree":"linear"}
+var rowTraitScales = {"height":"linear","clustering":"linear"}
