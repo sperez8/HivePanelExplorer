8a61d697dfc0fc784811474fb2e6b846b6666d09
spr <spr@pixmob.com>
2020-10-17 19:31:40 -0400

    Makes compatible with python 2 and 3 and reformats main scripts

--- hivepanel/create_panel.py
+++ hivepanel/create_panel.py
@@ -1,120 +1,124 @@
-'''
+"""
 created  10/06/2014
 
 by sperez
 
 makes hive panel from user input
-'''
+"""
 
-import sys
-import os
+from __future__ import print_function
 import argparse
 import shutil
-import string
 from ntpath import basename, dirname
 from os.path import splitext
 from utilities_panel import *
 from file_skeletons import parameters_file, html_file
 
-PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),'methodfiles')
+PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'methodfiles')
 
 NODE_MEASURES = [degree,
-                nx.clustering,
-                nx.betweenness_centrality, 
-                nx.closeness_centrality,
-                component_membership,
-                ]
+                 nx.clustering,
+                 nx.betweenness_centrality,
+                 nx.closeness_centrality,
+                 component_membership,
+                 ]
 
 EDGE_MEASURES = [nx.edge_betweenness_centrality]
 
 NUM_AXES = 3
 
+
 def get_all_attributes(G):
     '''iters through nodes and edges to get their attributes'''
     nodeKeys = []
     edgeKeys = []
-    
+
     for n in G.nodes():
         nodeKeys.extend(G.node[n].keys())
     nodeKeys = set(nodeKeys)
 
-    for s,t in G.edges():
+    for s, t in G.edges():
         edgeKeys.extend(G[s][t].keys())
     edgeKeys = set(edgeKeys)
 
     return list(nodeKeys), list(edgeKeys)
 
+
 def make_js_files(G):
     '''make a network in js format'''
-    nodeAttributes = G.graph['nodeAttributes'] #names of attributes
+    nodeAttributes = G.graph['nodeAttributes']  # names of attributes
     edgeAttributes = G.graph['edgeAttributes']
     graphName = G.graph['title']
 
-    newnodeFile = os.path.join(G.graph['folder'],"{0}_nodes.js".format(graphName))
-    newedgeFile = os.path.join(G.graph['folder'],"{0}_edges.js".format(graphName))
+    newnodeFile = os.path.join(G.graph['folder'], "{0}_nodes.js".format(graphName))
+    newedgeFile = os.path.join(G.graph['folder'], "{0}_edges.js".format(graphName))
 
     f = open(newnodeFile, 'w')
-    f.write('var SVGTitle = "{0} Hive Panel"\n'.format(graphName.replace('_',' ')))
+    f.write('var SVGTitle = "{0} Hive Panel"\n'.format(graphName.replace('_', ' ')))
     f.write('var nodes = [\n')
     nodes = G.nodes()
     for node in nodes:
-        line = '    {name: "' + str(node) +'"'
+        line = '    {name: "' + str(node) + '"'
         for attribute in nodeAttributes:
-            line  += ', '+attribute+': "' + str(G.node[node][attribute]) + '"'
+            line += ', ' + attribute + ': "' + str(G.node[node][attribute]) + '"'
         line += '},\n'
         f.write(line)
     f.write('];')
     f.close()
 
-    f = open(newedgeFile,'w')
+    f = open(newedgeFile, 'w')
     f.write('var links = [\n')
-    for (s,t) in G.edges():
-        line = '  {source: nodes['+str(nodes.index(s))+'], target: nodes['+str(nodes.index(t))+']'
+    for (s, t) in G.edges():
+        line = '  {source: nodes[' + str(nodes.index(s)) + '], target: nodes[' + str(nodes.index(t)) + ']'
         for attribute in edgeAttributes:
-            line  += ', '+attribute+': "' + str(G.edge[s][t][attribute]) + '"'
+            line += ', ' + attribute + ': "' + str(G.edge[s][t][attribute]) + '"'
         line += '},\n'
         f.write(line)
     f.write('];')
     f.close()
     return None
 
+
 def make_panel_parameters_file(G):
-    fileName = os.path.join(G.graph['folder'],"{0}_parameters.js".format(G.graph['title']))
+    fileName = os.path.join(G.graph['folder'], "{0}_parameters.js".format(G.graph['title']))
     f = open(fileName, 'w')
-    asgtraits = [G.graph['nodeAttributes'][0],G.graph['nodeAttributes'][2]]
-    postraits = [G.graph['nodeAttributes'][1],G.graph['nodeAttributes'][3]]
+    asgtraits = [G.graph['nodeAttributes'][0], G.graph['nodeAttributes'][2]]
+    postraits = [G.graph['nodeAttributes'][1], G.graph['nodeAttributes'][3]]
     newfile = parameters_file.format(G.graph['axes'],
-                    str(G.graph['double']).lower(),
-                    '"'    +   '","'.join(asgtraits)  +   '"',
-                    '"'    +   '","'.join(postraits)  +   '"',
-                    '"' +   '":"linear","'.join(asgtraits)    +  '":"linear"',
-                    '"' +   '":"linear","'.join(postraits)    +  '":"linear"',
-                    )
+                                     str(G.graph['double']).lower(),
+                                     '"' + '","'.join(asgtraits) + '"',
+                                     '"' + '","'.join(postraits) + '"',
+                                     '"' + '":"linear","'.join(asgtraits) + '":"linear"',
+                                     '"' + '":"linear","'.join(postraits) + '":"linear"',
+                                     )
     f.write(newfile)
     f.close()
     return None
 
+
 def make_html_file(G):
-    fileName = os.path.join(G.graph['folder'],"{0}_panel.html".format(G.graph['title']))
+    fileName = os.path.join(G.graph['folder'], "{0}_panel.html".format(G.graph['title']))
     f = open(fileName, 'w')
     newfile = html_file.format(G.graph['title'])
     f.write(newfile)
     f.close()
     return None
 
+
 def make_panel_methods(G):
     destination = G.graph['folder']
     fileName = "panel_methods.js"
-    shutil.copy(os.path.join(PATH,fileName),destination)
+    shutil.copy(os.path.join(PATH, fileName), destination)
     fileName = "panel_style.css"
-    shutil.copy(os.path.join(PATH,fileName),destination)
+    shutil.copy(os.path.join(PATH, fileName), destination)
     fileName = "d3.v3.min.js"
-    shutil.copy(os.path.join(PATH,fileName),destination)
+    shutil.copy(os.path.join(PATH, fileName), destination)
     fileName = "d3.hive.v0.min.js"
-    shutil.copy(os.path.join(PATH,fileName),destination)
+    shutil.copy(os.path.join(PATH, fileName), destination)
     fileName = "remove_icon.svg"
-    shutil.copy(os.path.join(PATH,fileName),destination)
+    shutil.copy(os.path.join(PATH, fileName), destination)
     return None
+
 
 def make_panel(G):
     make_js_files(G)
@@ -123,15 +127,17 @@ def make_panel(G):
     make_panel_methods(G)
     return None
 
+
 def main(*argv):
     '''handles user input and creates a panel'''
-    parser = argparse.ArgumentParser(description='This scripts takes networks and created the necessary file to make an interactive Hive panel')
-    #parser.add_argument('-input', help='Location of network file')
+    parser = argparse.ArgumentParser(
+        description='This scripts takes networks and created the necessary file to make an interactive Hive panel')
+    # parser.add_argument('-input', help='Location of network file')
     parser.add_argument('-nodes', help='Location of node network file')
     parser.add_argument('-edges', help='Location of edge network file')
     parser.add_argument('-title', help='Title/Name of graph')
     parser.add_argument('-folder', help='Output folder')
-    parser.add_argument('-format', help='Input format of network', default = 'txt')
+    parser.add_argument('-format', help='Input format of network', default='txt')
     args = parser.parse_args()
 
     # #Get graph in networkx format
@@ -141,7 +147,7 @@ def main(*argv):
     #     title = splitext(basename(args.nodes))[0]
     #     folder = dirname(args.input)
     # elif
-    if args.format=='txt':
+    if args.format == 'txt':
         print_message("Reading .txt as a networkx graph.")
         G = import_graph(args.nodes, args.edges)
         title = splitext(basename(args.nodes))[0]
@@ -151,45 +157,37 @@ def main(*argv):
     #     parser.print_help()
     #     sys.exit()
 
-
     if args.title:
         title = args.title
 
     if args.folder:
         folder = args.folder
 
-    #store all the plotting info in the graph as attributes
-    G.graph['axes']= NUM_AXES
-    G.graph['double']= False
-    G.graph['folder']=folder
-    G.graph['title']=title
-    G.graph['nodeAttributes'],G.graph['edgeAttributes']=get_all_attributes(G)
+    # store all the plotting info in the graph as attributes
+    G.graph['axes'] = NUM_AXES
+    G.graph['double'] = False
+    G.graph['folder'] = folder
+    G.graph['title'] = title
+    G.graph['nodeAttributes'], G.graph['edgeAttributes'] = get_all_attributes(G)
     for m in NODE_MEASURES:
         G.graph['nodeAttributes'].append(m.__name__)
         measures = m(G)
-        nx.set_node_attributes(G,m.__name__,measures)
+        nx.set_node_attributes(G, m.__name__, measures)
 
     for m in EDGE_MEASURES:
         G.graph['edgeAttributes'].append(m.__name__)
         measures = m(G)
-        nx.set_edge_attributes(G,m.__name__,measures)
+        nx.set_edge_attributes(G, m.__name__, measures)
 
-    print "PARAMETERS OF NETWORK:"
-    print "     Network name:", G.graph['title']
-    print "     Node attributes:\n\t\t", '\n\t\t'.join(G.graph['nodeAttributes'])
-    print "     Edge attributes:\n\t\t", '\n\t\t'.join(G.graph['edgeAttributes'])
-    print "     Output folder", G.graph['folder']
+    print("PARAMETERS OF NETWORK:")
+    print("     Network name:", G.graph['title'])
+    print("     Node attributes:\n\t\t", '\n\t\t'.join(G.graph['nodeAttributes']))
+    print("     Edge attributes:\n\t\t", '\n\t\t'.join(G.graph['edgeAttributes']))
+    print("     Output folder", G.graph['folder'])
 
     print_message('Making panel.')
-    make_panel(G,)
-
+    make_panel(G, )
 
 
 if __name__ == "__main__":
     main(*sys.argv[1:])
-
-
-
-
-
-
--- hivepanel/utilities_panel.py
+++ hivepanel/utilities_panel.py
@@ -5,84 +5,92 @@ by sperez
 
 functions used by create_panel
 '''
+from __future__ import print_function
 
 import sys
 import os
 import numpy as np
 import string
 
-#Loading local copy of networkx package
+# Loading local copy of networkx package
 sys.path.insert(0, os.path.abspath(".."))
 import networkx as nx
 
-
 ADD_LONELY_NODES = False
+
 
 def degree(G):
     return G.degree()
 
+
 def component_membership(G):
     nodeComponents = {}
-    #get connected components sorted by size
-    ccs = sorted(nx.connected_components(G), key = len, reverse=True)
-    for i,cc in enumerate(ccs):
+    # get connected components sorted by size
+    ccs = sorted(nx.connected_components(G), key=len, reverse=True)
+    for i, cc in enumerate(ccs):
         for node in cc:
-            nodeComponents[node]=i
+            nodeComponents[node] = i
     return nodeComponents
 
+
 def import_graphml(graphmlFile):
-    #parse graphml file
+    # parse graphml file
     G = nx.read_graphml(graphmlFile)
     return G.to_undirected(reciprocal=False)
 
-def import_graph(nodeFile, edgeFile, filterEdges = True):
+
+def import_graph(nodeFile, edgeFile, filterEdges=True):
     '''make a networkx graph from a csv or tsv'''
-    
+
     nodes, nodeAttributes = get_nodes(nodeFile)
     sources, targets, edgeAttributes = get_edges(edgeFile)
-    
+
     G = make_graph(sources, targets, nodes, filterEdges, ADD_LONELY_NODES)
     allNodes = G.nodes()
-    for i,n in enumerate(nodes):
-        for p,v in nodeAttributes.iteritems():
+    for i, n in enumerate(nodes):
+        for p in nodeAttributes.keys():
+            v = nodeAttributes[p]
             if not ADD_LONELY_NODES and n not in allNodes:
                 continue
             G.node[n][p] = v[i]
 
-    for i,e in enumerate(zip(sources, targets)):
-        s,t = e[0],e[1]
+    for i, e in enumerate(zip(sources, targets)):
+        s, t = e[0], e[1]
         if filterEdges:
             if s not in nodes or t not in nodes:
                 continue
-        for p,v in edgeAttributes.iteritems():
+        for p in edgeAttributes.keys():
+            v = edgeAttributes[p]
             G[s][t][p] = v[i]
     return G
+
 
 def convert_graphml(graphmlFile):
     G = import_graphml(graphmlFile)
     fileName = graphmlFile.split('.graphml')[0]
-    convert_graph(G,fileName)
+    convert_graph(G, fileName)
     return None
 
-def convert_graph(G,fileName):
+
+def convert_graph(G, fileName):
     sources, targets, edgeAttributes = zip(*G.edges(data=True))
-    nodeFile = fileName+'_nodes.csv'
-    edgeFile = fileName+'_edges.csv'
-    nf = open(nodeFile,'w')
+    nodeFile = fileName + '_nodes.csv'
+    edgeFile = fileName + '_edges.csv'
+    nf = open(nodeFile, 'w')
     keys = []
     for node, nodeAttributes in G.nodes(data=True):
         new_keys = nodeAttributes.keys()
         if new_keys != keys:
             keys.extend(new_keys)
-    
+
     keys = set(keys)
-    
-    #write header
+
+    # write header
     if keys:
-        nf.write('Node'+','+','.join(keys))
+        nf.write('Node' + ',' + ','.join(keys))
     else:
-        nf.write('Node')        
-    
+        nf.write('Node')
+
     for node, nodeAttributes in G.nodes(data=True):
         row = []
         row.append(node)
@@ -92,21 +100,21 @@ def convert_graph(G,fileName):
             else:
                 row.append('None')
         nf.write('\n' + ','.join([str(r) for r in row]))
-    
+
     nf.close()
-    
-    ef = open(edgeFile,'w')
+
+    ef = open(edgeFile, 'w')
     keys = []
-    for source,target, edgeAttributes in G.edges(data=True):
+    for source, target, edgeAttributes in G.edges(data=True):
         new_keys = edgeAttributes.keys()
         if new_keys != keys:
             keys.extend(new_keys)
-    
+
     keys = set(keys)
-    
+
     if keys:
-        ef.write('source' + ',' + 'target' + ',' +','.join(keys))
-    else: 
+        ef.write('source' + ',' + 'target' + ',' + ','.join(keys))
+    else:
         ef.write('source' + ',' + 'target')
 
     for source, target, edgeAttributes in G.edges(data=True):
@@ -119,18 +127,18 @@ def convert_graph(G,fileName):
             else:
                 row.append('None')
         ef.write('\n' + ','.join([str(r) for r in row]))
-    
+
     print_message("writing nodefile", nodeFile)
     print_message("writing edgefile", edgeFile)
     return None
 
 
-def make_graph(sources, targets, nodes, filterEdges= True, addLonelyNodes = False):
+def make_graph(sources, targets, nodes, filterEdges=True, addLonelyNodes=False):
     '''Makes a graph using the networkx package Graph instance'''
     G = nx.Graph()
     if addLonelyNodes:
-        G.add_nodes_from(nodes) #add all nodes, even ones without edges...
-    G.add_edges_from(zipper(sources,targets))
+        G.add_nodes_from(nodes)  # add all nodes, even ones without edges...
+    G.add_edges_from(zipper(sources, targets))
     if filterEdges:
         for n in G.nodes():
             if n not in nodes:
@@ -138,50 +146,51 @@ def make_graph(sources, targets, nodes, filterEdges= True, addLonelyNodes = Fals
 
     return G
 
+
 def load_file(inputFile):
-    #try loading as a tab delimited file, otherwise a comma delimited file.
+    # try loading as a tab delimited file, otherwise a comma delimited file.
     try:
-        data = np.genfromtxt(inputFile, delimiter='\t', dtype='str', filling_values = 'None')
-        attribute = data[0,1:] #this will fail if file is comma
+        data = np.genfromtxt(inputFile, delimiter='\t', dtype='str', filling_values='None')
+        attribute = data[0, 1:]  # this will fail if file is comma
     except IndexError:
         try:
-            data = np.genfromtxt(inputFile, delimiter=',', dtype='str', filling_values = 'None')
-            attribute = data[0,1:]
+            data = np.genfromtxt(inputFile, delimiter=',', dtype='str', filling_values='None')
+            attribute = data[0, 1:]
         except IndexError:
             print_message("ERROR: Please use a tab delimited or comma delimited text file")
             sys.exit()
     return data
 
 
-def get_nodes(inputFile,removeNA=None):
+def get_nodes(inputFile, removeNA=None):
     '''gets nodes and their attribute from csv file'''
 
     data = load_file(inputFile)
-    attribute = data[0,1:]
+    attribute = data[0, 1:]
 
-    #get attribute and format as strings
+    # get attribute and format as strings
     attribute = format_attribute(attribute)
-    
-    if removeNA:
-        colName = nx.betweenness_centrality.__name__.replace('_',' ').capitalize()
-        col = np.where(data[0,:]==colName)[0][0]
-        #remove first row with column names
-        data = data[1:,]
-        data = data[np.where(data[:,col]!=removeNA)]
-    else:
-        #remove first row with column names
-        data = data[1:,]
 
-    #get all the node data
-    nodes = list(data[:,0])
-    
-    #take note of number of nodes
+    if removeNA:
+        colName = nx.betweenness_centrality.__name__.replace('_', ' ').capitalize()
+        col = np.where(data[0, :] == colName)[0][0]
+        # remove first row with column names
+        data = data[1:, ]
+        data = data[np.where(data[:, col] != removeNA)]
+    else:
+        # remove first row with column names
+        data = data[1:, ]
+
+    # get all the node data
+    nodes = list(data[:, 0])
+
+    # take note of number of nodes
     totalNodes = len(nodes)
-    
-    #transform node attribute into the numerical types if possible
+
+    # transform node attribute into the numerical types if possible
     nodeAttributes = {}
 
-    for i, column in enumerate(data[:,1:].T):
+    for i, column in enumerate(data[:, 1:].T):
         values = convert_type(list(column))
         nodeAttributes[attribute[i]] = values
     nodeAttributes = nodeAttributes
@@ -191,72 +200,77 @@ def get_nodes(inputFile,removeNA=None):
 
 def get_edges(inputFile):
     '''gets edges and their attribute from csv file'''
-    
+
     data = load_file(inputFile)
-    
-    #get attribute and format as strings
-    attribute = data[0,2:]
+
+    # get attribute and format as strings
+    attribute = data[0, 2:]
     attribute = format_attribute(attribute)
-    
-    #remove first row with column names
-    data = data[1:,]
-    
-    #get all the edge data
-    sources = list(data[:,0])        
-    targets = list(data[:,1])
-    
-    #take note of number of edges:
+
+    # remove first row with column names
+    data = data[1:, ]
+
+    # get all the edge data
+    sources = list(data[:, 0])
+    targets = list(data[:, 1])
+
+    # take note of number of edges:
     totalEdges = len(sources)
 
-    #transform edge attribute into the numerical types if possible
+    # transform edge attribute into the numerical types if possible
     edgeAttributes = {}
 
-    for i, column in enumerate(data[:,2:].T):
+    for i, column in enumerate(data[:, 2:].T):
         values = convert_type(list(column))
         edgeAttributes[attribute[i]] = values
     edgeAttributes = edgeAttributes
-    
-    #store the name of the edge attribute
+
+    # store the name of the edge attribute
     edgePropertyList = edgeAttributes.keys()
 
     return sources, targets, edgeAttributes
 
+
 def convert_type(data):
-    def num(s):
+    def num(d):
         '''convert list of strings to corresponding int or float type'''
         try:
             return int(d)
         except ValueError:
             return float(d)
-    
+
     try:
         convertedData = [num(d) for d in data]
         return convertedData
     except ValueError:
         return data
 
-def format_attribute(attribute, debug = False):
+
+def format_attribute(attribute, debug=False):
     '''takes a list of attribute names and removes all punctuation and numbers'''
-    
-    numbers = {1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine', 10:'ten'}
-    
+
+    numbers = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine',
+               10: 'ten'}
+
     def convert_word(word):
         '''remove punctuation and numbers from a word'''
         w = word
-        word = '_'.join(word.split()) #removes all whitespace (tabs, newlines, spaces...)
-        for c in string.punctuation.replace('_','') + string.digits:
-            word = word.replace(c,'')
+        word = '_'.join(word.split())  # removes all whitespace (tabs, newlines, spaces...)
+        for c in string.punctuation.replace('_', '') + string.digits:
+            word = word.replace(c, '')
         if w != word:
             if debug:
-                print_message("The attribute \'{0}\' contains spaces, punctuation or digits and has been renamed '{1}'".format(w,word))
+                print_message(
+                    "The attribute \'{0}\' contains spaces, punctuation or digits and has been renamed '{1}'".format(w,
+                                                                                                                     word))
         return word
-         
+
     newAttributes = []
     i = 1
     for prop in attribute:
         newProp = convert_word(prop)
         if not newProp:
-            #if attribute isn't named, we give it one
+            # if attribute isn't named, we give it one
             newAttributes.append('unNamedProperty' + numbers[i] + '')
             i += 1
         elif newProp in newAttributes:
@@ -265,20 +279,18 @@ def format_attribute(attribute, debug = False):
             newAttributes.append(newProp)
     return newAttributes
 
+
 def zipper(*args):
     '''a revamped version of zip() method that checks that lists
     to be zipped are the same length'''
-    for i,item in enumerate(args):
+    for i, item in enumerate(args):
         if len(item) != len(args[0]):
             raise ValueError('The lists to be zipped aren\'t the same length.')
-    
+
     return zip(*args)
+
 
 def print_message(*args):
     '''Formats terminal messages for user'''
-    print "\n*\n ", ' '.join(args),"\n"
+    print("\n*\n ", ' '.join(args), "\n")
     return None
-
-
-
-
